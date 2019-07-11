1aa81285cb2cb5ea0a25ef2f29b5e40ba67b819d
Jack Sparroy <mr.vadim00290@gmail.com>
2019-07-11 14:20:31 +0300

    add Connection pool database,properties

--- sql/fill_tables.sql
+++ sql/fill_tables.sql
@@ -1,23 +1,21 @@
 USE `meetups`;
 
 INSERT INTO `user` (
-	`id`,
 	`login`,
 	`password`,
 	`permission`
 ) VALUES (
-	1,
-	"root1",
+
+	"root2",
 	"1234", /* MD5 хэш пароля "user" */
 	1
 ), (
-	2,
-	"user3",
+
+	"user5",
 	"EE11CBB19052E40B07AAC0CA060C23EE", /* MD5 хэш пароля "user" */
 	2
 ), (
-	3,
-	"user4",
+	"user6",
 	"EE11CBB19052E40B07AAC0CA060C23EE", /* MD5 хэш пароля "user" */
 	2
 );
